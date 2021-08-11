f1cb58cd24d0f9480398676e735c0bf45fb69e0e
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-08-11 19:13:19 +0700

    up code

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
-
+import axios from 'axios';
 export default class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            products:[]
+        }
+    }
+    componentDidMount() { // sẽ thực thi sau khi render lần đầu tiên
+        // lấy dữ liệu từ api để nạp cho products trong state
+        axios.get("https://foodgroup.herokuapp.com/api/today-special")
+            .then(rs=>{
+                this.setState({
+                    products:rs.data.data
+                })
+            })
+    }
+
     render() {
+        const products = this.state.products;
         return (
+            <div>
             <div className="row">
                 <div className="col-lg-3 col-6">
                     <div className="small-box bg-info">
@@ -56,6 +74,35 @@ export default class Home extends React.Component{
                         <a href="#" className="small-box-footer">More info <i className="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
+            </div>
+            <div className="row">
+                <section className="col-12">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            products.map((e,k)=>{
+                                return <tr key={k}>
+                                    <td>{e.id}</td>
+                                    <td>{e.name}</td>
+                                    <td><img src={e.image} width={50}/></td>
+                                    <td>{e.description}</td>
+                                    <td>{e.price}</td>
+                                </tr>
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </section>
+            </div>
             </div>
         )
     }
